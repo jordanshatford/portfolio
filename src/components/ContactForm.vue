e741bfcc842957ae3e8c76e89e1e7c92a03c0b8e
Jordan Shatford <jordanshatford@live.com>
2022-03-27 11:30:18 -0300

    remove: unused bootstrap scss files

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,17 +1,17 @@
 <template>
   <form @submit.prevent="onSubmit" role="form" class="contactform">
     <div class="row">
-      <div class="col-md-6 form-group">
+      <div class="col-md-6">
         <v-input type="text" name="name" id="name" placeholder="Your Name" v-model="form.name" required></v-input>
       </div>
-      <div class="col-md-6 form-group mt-3 mt-md-0">
+      <div class="col-md-6 mt-3 mt-md-0">
         <v-input type="email" name="email" id="email" placeholder="Your Email" v-model="form.email" required></v-input>
       </div>
     </div>
-    <div class="form-group mt-3">
+    <div class="mt-3">
       <v-input type="text" name="subject" id="subject" placeholder="Subject" required v-model="form.subject"></v-input>
     </div>
-    <div class="form-group mt-3">
+    <div class="mt-3">
       <v-textarea name="message" :rows="5" placeholder="Message" required v-model="form.message"></v-textarea>
     </div>
     <div class="contactform__alert">
